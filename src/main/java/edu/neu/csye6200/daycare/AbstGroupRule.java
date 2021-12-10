ba0b7f1c20be7c163150ac681b23b885a253a63a
Louie Li <lizongyao9745@hotmail.com>
2021-12-10 18:56:49 -0500

    Updated

--- src/main/java/edu/neu/csye6200/daycare/AbstGroupRule.java
+++ src/main/java/edu/neu/csye6200/daycare/AbstGroupRule.java
@@ -3,7 +3,7 @@ package edu.neu.csye6200.daycare;
 import java.util.*;
 
 public abstract class AbstGroupRule {
-	public abstract void assign(Student s, Teacher t);
+	public abstract int assign(Student s, Teacher t);	// return 0 if success, else return -1.
 	public abstract Map<Integer,List<Group>> generateGroups(Teachers ts, Students ss);
 	
 }
